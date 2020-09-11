05f8cf101ff9964ce8a079153729e026d8a2c49c
Aayu8982 <aayu8982@gmail.com>
2020-09-11 11:58:27 +0530

    deploy to special repository

---
+++ precache-manifest.b0c55a93f354d40ce7be519dc953f622.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "006180f16c08deb3d99cc929801abd9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf40441c78f191425be6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b81c5da4404ded0b9323",
+    "url": "/static/js/2.97bb4f35.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.97bb4f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf40441c78f191425be6",
+    "url": "/static/js/main.abdfbe5c.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
